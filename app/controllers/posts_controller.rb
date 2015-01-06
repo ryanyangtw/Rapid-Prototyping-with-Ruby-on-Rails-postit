fc5994f666c556cf0f8001618038f85d2bd000ce
Ryan <hiphubryan@gmail.com>
2015-01-06 23:12:08 +0800

    Finish voting by ajax

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -47,12 +47,19 @@ class PostsController < ApplicationController
   def vote
 
     @vote = Vote.create(voteable: @post, creator: current_user, vote: params[:vote])
-    if @vote.valid?
-      flash[:notice] = 'Your vote was counted.'
-    else
-      flash[:error] = 'You can only vote on a post once.'
+    
+    respond_to do |format|
+      format.html do
+        if @vote.valid?
+          flash[:notice] = 'Your vote was counted.'
+        else
+          flash[:error] = 'You can only vote on a post once.'
+        end
+        redirect_to :back
+      end
+      # Add fotmat.js to handle request from js(ajax)
+      format.js
     end
-    redirect_to :back
   end
 
   
